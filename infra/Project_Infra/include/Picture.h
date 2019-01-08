f59767bc720b6c0321ad16c6e9a5292c31ad70b1
triloa <46493219+triloa@users.noreply.github.com>
2019-01-08 17:03:15 +0100

    Add files via upload

---
+++ infra/Project_Infra/include/Picture.h
@@ -0,0 +1,51 @@
+#ifndef PICTURE_H
+#define PICTURE_H
+
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include <stdlib.h>
+
+class Picture{
+  private:
+    cv::Mat picture;
+
+  public:
+    Picture(const std::string& filename);
+
+    Picture(const cv::Mat& pic);
+
+    float get_intensity(unsigned int i, unsigned int j)const;
+
+    void set_intensity(unsigned int i, unsigned int j,float intensity);
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ infra/Project_Infra/include/Usefull_functions.h
@@ -0,0 +1,29 @@
+#ifndef USEFULL_FUNCTIONS_H
+#define USEFULL_FUNCTIONS_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
